b6d1a7e5f37a6e587e9ab3230d7f697cf77cf90d
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2023-10-27 07:24:51 +0100

    Refactoring.

--- fetchOpenOrders.js
+++ fetchOpenOrders.js
@@ -12,35 +12,32 @@ const crypto = require('crypto');
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
 
-const createSignature = (query) => {
+function createSignature(query) {
     return crypto.createHmac('sha256', API_SECRET).update(query).digest('hex');
 }
-
-const fetchOpenOrders = async (symbol) => {
+async function fetchOpenOrders(symbol) {
     const endpoint = '/api/v3/openOrders';
     const timestamp = Date.now();
     const query = `symbol=${symbol}&timestamp=${timestamp}`;
-    const signature = crypto.createHmac('sha256', API_SECRET).update(query).digest('hex');
+    const signature = createSignature(query);
     const url = `https://api.binance.com${endpoint}?${query}&signature=${signature}`;
-    
-    const response = await axios({
-        method: 'GET',
-        url: url,
+
+    const response = await axios.get(url, {
         headers: {
             'X-MBX-APIKEY': API_KEY
         }
     });
     return response.data.map(d => ({
-        orderId : d.orderId,
-        side    : d.side,
-        origQty : d.origQty,
-        symbol  : d.symbol,
-        price   : d.price
-    }));   
-};
-const fetchAndLogOrders = async (symbol) => {
+        orderId: d.orderId,
+        side: d.side,
+        origQty: d.origQty,
+        symbol: d.symbol,
+        price: d.price
+    }));
+}
+async function fetchAndLogOrders(symbol) {
     const orders = await fetchOpenOrders(symbol);
-    console.log(`${orders.length} ${symbol} open orders`, new Date());   
+    console.log(`${orders.length} open orders for ${symbol} at `, new Date());
     console.log(orders);
 }
 fetchAndLogOrders('ETHUSDC');
