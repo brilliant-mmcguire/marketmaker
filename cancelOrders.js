23799425502bbda9787636639a350604d633b1a7
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2023-10-30 21:53:42 +0000

    Extract cancelOpenOrders as a function.

--- cancelOrders.js
+++ cancelOrders.js
@@ -1,9 +1,23 @@
 /*
 We can cancel orders for a given symbol to clear them out before placing new orders.
-https://binance-docs.github.io/apidocs/spot/en/#cancel-order-trade
 */
 const { cancelOrder } = require('./orderTxns');
 const { fetchOpenOrders } = require('./orderTxns');
+
+async function cancelOpenOrders(symbol) {
+    const orders = await fetchOpenOrders(symbol);
+    if(orders.length==0) {
+        console.log(`No orders to cancel.`);
+        return;
+    }
+    
+    console.log(`Cancelling orders: ${orders}`);
+    orders.forEach(order => {
+        cancelOrder(order.symbol, order.orderId).then(response => {
+            console.log(`Cancelled order ${order.orderId}:`, response);
+        });
+    });    
+}  
 async function main(){
     const symbol = process.argv[2];
     if(!symbol) {
@@ -11,19 +25,7 @@ async function main(){
         return; 
     }
     try {
-        const orders = await fetchOpenOrders(symbol);
-        if(orders.length==0) {
-            console.log(`No orders to cancel.`);
-            return;
-        }
-
-        console.log(`Cancelling orders: ${orders}`);
-        
-        orders.forEach(order => {
-            cancelOrder(order.symbol, order.orderId).then(response => {
-                console.log(`Cancelled order ${order.orderId}:`, response);
-            });
-        });
+        cancelOpenOrders(symbol);
     } catch (error) {    
         console.error(`Error cancelling orders: ${error}`);
     }
