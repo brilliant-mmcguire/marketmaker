7203471c6885bfa2afd58c8e64f7f61e65c4c02b
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2023-11-01 17:50:46 +0000

    Move to hourly cron.

--- cronOrders.js
+++ cronOrders.js
@@ -2,6 +2,10 @@ const cron = require('node-cron');
 
 // Schedule the task to run every second
 console.log('Placing orders for it');
-const jobA = cron.schedule('* * * * *', () => {
-    console.log('Every minute: ' + new Date().toLocaleString() );
+//const jobA = cron.schedule('* * * * *', () => {
+//    console.log('Every minute: ' + new Date().toLocaleString() );
+//});
+const jobB = cron.schedule('0 * * * *', () => {
+    console.log('Every hour: ' + new Date().toLocaleString());
 });
+jobB.now();
