70044b299dfc09600baac2a4ae6cb937debb209e
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2023-11-02 09:32:49 +0000

    cronOrder: expect symbol as argument.

--- cronOrders.js
+++ cronOrders.js
@@ -2,6 +2,9 @@ const cron = require('node-cron');
 const { cancelOpenOrders } = require('./replaceOrders');
 const { placeNewOrders } = require('./replaceOrders');
 
+const symbol = process.argv[2];
+if(!symbol) throw "Symbol not provided.";
+
 // Schedule the task to run every hour at 13 minutes past the hour.
 const jobB = cron.schedule('13 * * * *', async () => {
     console.log('Invoking hourly task at ' + new Date().toLocaleString());
