6d856418f04162bfe702b61bbf8b5918e4d526aa
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2023-11-30 11:20:23 +0000

    Fetch trades for the last 24 hours only.

--- fetchTrades.js
+++ fetchTrades.js
@@ -6,6 +6,7 @@ https://binance-docs.github.io/apidocs/spot/en/#account-trade-list-user_data
 const cfg = require('dotenv').config();
 const axios = require('axios');
 const crypto = require('crypto');
+const qs = require('qs');
 
 const API_KEY = process.env.API_KEY;
 const API_SECRET = process.env.API_SECRET;
@@ -16,8 +17,20 @@ function createSignature(query) {
 }
 async function fetchMyTrades(symbol, limit) {
     const endpoint = '/api/v3/myTrades';
+
+
     const timestamp = Date.now();
-    const query = `symbol=${symbol}&limit=${limit}&timestamp=${timestamp}`;
+    const params = {
+        symbol: symbol,
+        timestamp: timestamp,
+        limit: limit,
+        // Add other necessary parameters like startTime and endTime for the last 24 hours
+        startTime: Date.now() - 24 * 60 * 60 * 1000, // 24 hours ago
+    };
+//    const startTime = Date.now() - 24 * 60 * 60 * 1000; // 24 hours ago
+
+    const query = qs.stringify(params);
+    //const query = `symbol=${symbol}&limit=${limit}&timestamp=${timestamp}&startTime=${startTime}`;
     const signature = createSignature(query);
     const url = `${BASE_URL}${endpoint}?${query}&signature=${signature}`;
     const response = await axios.get(url, {
@@ -52,14 +65,13 @@ function computePosition(trades) {
 exports.fetchPositions = fetchPositions;
 async function fetchPositions(symbol) {
     try {
-        const trades = await fetchMyTrades(symbol, 50);
+        const trades = await fetchMyTrades(symbol, 1000);
         const positions = {
             symbol : symbol,
-            long  : computePosition(trades.buys),
-            short : computePosition(trades.sells)
+            sold : computePosition(trades.sells),
+            bought  : computePosition(trades.buys)
         };
         console.log(positions);
-        
     } catch (error) {
         console.error(`Error fetching trades: ${error}`);
     }
@@ -69,5 +81,7 @@ async function main() {
     if (require.main !== module) return;
     fetchPositions('BTCUSDC');
     fetchPositions('ETHUSDC');
+   // fetchPositions('USDCUSDT');
 }
+
 main();
