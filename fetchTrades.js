070e17293b69a8521808fce82111a4d689dd556e
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2023-12-13 10:36:00 +0000

    Position calculations treating trades in time order.

--- fetchTrades.js
+++ fetchTrades.js
@@ -15,6 +15,7 @@ const BASE_URL = 'https://api.binance.com';
 function createSignature(query) {
     return crypto.createHmac('sha256', API_SECRET).update(query).digest('hex');
 }
+
 async function fetchMyTrades(symbol, limit) {
     const endpoint = '/api/v3/myTrades';
     const ts = new Date();
@@ -36,10 +37,12 @@ async function fetchMyTrades(symbol, limit) {
         }
     });
     return {
-        buys: response.data.filter(trade => trade.isBuyer),
-        sells: response.data.filter(trade => !trade.isBuyer)
+        all    : response.data, 
+        buys   : response.data.filter(trade => trade.isBuyer),
+        sells  : response.data.filter(trade => !trade.isBuyer)
     };
 }
+
 function computePosition(trades) {
     const totalQty = trades
         .reduce((sum, trade) => sum + parseFloat(trade.qty), 0);
@@ -59,11 +62,78 @@ function computePosition(trades) {
         avgPrice: totalValue / totalQty
     };
 }
+
+async function fetchPositions2(symbol) {
+    try {
+        const trades = await fetchMyTrades(symbol, 1000);
+        const pos = {
+            symbol : symbol,
+            qty : 0.0,
+            quoteQty : 0.0,
+            avgPrice : 0.0,
+            cost: 0.0,
+            matchedQty : 0.0,
+            realisedPL : 0.0,
+            sold : computePosition(trades.sells),
+            bought  : computePosition(trades.buys)
+        };
+
+        for(let i = 0; i < trades.all.length; i++) {
+            let t = trades.all[i];
+            if(t.isBuyer) {  
+                if(pos.qty >= 0) { // opening trade on long position. 
+                    pos.qty += parseFloat(t.qty);
+                    pos.quoteQty += parseFloat(t.quoteQty);    
+                    pos.cost += parseFloat(t.qty)*parseFloat(t.price);
+                } else {  // closing trade on short position.
+                    if(parseFloat(t.qty)>pos.qty) {  // crosses zero position. 
+                        pos.qty += parseFloat(t.qty);
+                        pos.quoteQty += parseFloat(t.quoteQty);
+                        pos.cost += pos.qty*avgPrice; // zero out cost.
+                        pos.realisedPL += (parseFloat(t.price) - pos.avgPrice)*pos.qty;         
+                        pos.cost += (parseFloat(t.qty) - pos.qty)*parseFloat(t.price)
+                    } else { 
+                        pos.qty += parseFloat(t.qty);
+                        pos.quoteQty += parseFloat(t.quoteQty);
+                        pos.cost += parseFloat(t.qty)*pos.avgPrice;
+                        pos.realisedPL += (parseFloat(t.price) - pos.avgPrice)*parseFloat(t.qty); 
+                    }
+                    pos.avgPrice = (pos.qty == 0.0) ?  0 : pos.cost / pos.qty;
+                }
+            } else {  // seller. 
+                if(pos.qty <= 0) { // opening sell trade on short position. 
+                    pos.qty -= parseFloat(t.qty);
+                    pos.quoteQty -= parseFloat(t.quoteQty);    
+                    pos.cost -= parseFloat(t.qty)*parseFloat(t.price);
+                } else {  // closing sell trade on long position.
+                    if(parseFloat(t.qty)>pos.qty) {  // crosses zero position. 
+                        pos.qty -= parseFloat(t.qty);
+                        pos.quoteQty -= parseFloat(t.quoteQty);
+                        pos.cost -= pos.qty*avgPrice; // zero out cost.
+                        pos.realisedPL += (parseFloat(t.price) - pos.avgPrice)*pos.qty;         
+                        pos.cost -= (parseFloat(t.qty) - pos.qty)*parseFloat(t.price)
+                    } else { 
+                        pos.qty -= parseFloat(t.qty);
+                        pos.quoteQty -= parseFloat(t.quoteQty);
+                        pos.cost -= parseFloat(t.qty)*pos.avgPrice;
+                        pos.realisedPL -= (parseFloat(t.price) - pos.avgPrice)*parseFloat(t.qty); 
+                    }
+                    pos.avgPrice = (pos.qty == 0.0) ?  0 : pos.cost / pos.qty;
+                }
+            }
+        };
+        
+        console.log(pos);
+        return pos;
+    } catch (error) {
+        console.error(`Error fetching trades: ${error}`);
+    }
+}
+
 exports.fetchPositions = fetchPositions;
 async function fetchPositions(symbol) {
     try {
         const trades = await fetchMyTrades(symbol, 1000);
-
         const position = {
             symbol : symbol,
             qty : 0.0,
@@ -96,7 +166,7 @@ async function main() {
     if (require.main !== module) return;
     const symbol = process.argv[2];
     if(!symbol) throw 'Symbol not provided.'; 
-    fetchPositions(symbol);
+    fetchPositions2(symbol);
 }
 
 if (require.main === module) main();
