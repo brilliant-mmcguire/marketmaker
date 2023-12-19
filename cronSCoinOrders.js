e3548b25c81bb1cd639290a2c531fee2e21a5da2
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2023-12-19 16:10:14 +0000

    Cron USDCUSDT orders placement.

---
+++ cronSCoinOrders.js
@@ -0,0 +1,15 @@
+const cron = require('node-cron');
+const { placeSCoinOrders } = require('./replaceSCoinOrders');
+
+console.log("Starting cron: placing StableCoin orders for USDCUSCT."); 
+
+// Schedule the task to run every hour at 13 minutes past the hour.
+const job = cron.schedule('*/12 * * * *', async () => {
+    console.log('Invoking  task at ' + new Date().toLocaleString());
+    try {
+        await placeSCoinOrders();   
+    } catch (error) {    
+        console.error(`Error placing orders: ${error}`);
+    }
+});
+job.now(); // For immediate feedback when run from the console.
