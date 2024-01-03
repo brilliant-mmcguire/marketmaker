812d8c741c8adea648825b6000bffa61bf84a89a
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2024-01-03 22:06:13 +0000

    Cancel outstanding orders every 3 hours.

--- cronSCoinOrders.js
+++ cronSCoinOrders.js
@@ -1,11 +1,21 @@
 const cron = require('node-cron');
 const { placeSCoinOrders } = require('./replaceSCoinOrders');
+const { cancelOpenOrders } = require('./replaceOrders');
 
 console.log("Starting cron: placing StableCoin orders for USDCUSCT."); 
 
+const jobCancelOrders = cron.schedule('13 */3 * * *', async () => {
+    console.log('Invoking task at ' + new Date().toLocaleString());
+    try {
+        await cancelOpenOrders('USDCUSDT');   
+    } catch (error) {    
+        console.error(`Error placing orders: ${error}`);
+    }
+});
+
 // Schedule the task to run every hour at 13 minutes past the hour.
 const job = cron.schedule('*/5 * * * *', async () => {
-    console.log('Invoking  task at ' + new Date().toLocaleString());
+    console.log('Invoking place orders at ' + new Date().toLocaleString());
     try {
         await placeSCoinOrders();   
     } catch (error) {    
@@ -13,3 +23,4 @@ const job = cron.schedule('*/5 * * * *', async () => {
     }
 });
 job.now(); // For immediate feedback when run from the console.
+
