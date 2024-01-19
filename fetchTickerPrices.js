dcd2e4321a3a8ae369a9ef43618aa60a2226b973
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2024-01-19 20:24:58 +0000

    Test call for rolling price stats.

--- fetchTickerPrices.js
+++ fetchTickerPrices.js
@@ -1,4 +1,5 @@
 const { fetchLastPrice, fetchAvgPrice } = require('./marketDataTxns');
+const { fetchPriceStats } = require('./marketDataTxns');
 
 async function fetchCurrentPrice(symbol, i, s) {
     let lastPrice = await fetchLastPrice(symbol);
@@ -6,6 +7,12 @@ async function fetchCurrentPrice(symbol, i, s) {
     return lastPrice;
 }
 async function main() {
-    ['BTCUSDC', 'ETHUSDC'].forEach(fetchCurrentPrice);
+    //['BTCUSDC', 'ETHUSDC'].forEach(fetchCurrentPrice);
+    if (require.main !== module) return;
+    const symbol = process.argv[2];
+    if(!symbol) throw 'Symbol not provided.'; 
+    const priceStats = await fetchPriceStats(symbol);
+    console.log(priceStats, '1h');
 }    
-main();
+if (require.main === module) main();
+
