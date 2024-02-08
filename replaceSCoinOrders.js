b566ab2020caf9e5badbd39929616a3448a4c550
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2024-02-08 22:16:20 +0000

    Fix qtyQuanta value.

--- replaceSCoinOrders.js
+++ replaceSCoinOrders.js
@@ -42,7 +42,7 @@ This is the help regulate the rate of exection of our orders.
 The more orders are in queue ahead of us, the more orders we need to keep in the queue.
 The goal is to maintin a steady rate of execution and to baclance to rate of buy and sell trades. 
 */
-const qtyQuanta = [400000, 10000000, 2500000, 7500000, 15000000];
+const qtyQuanta = [400000, 1000000, 2500000, 7500000, 15000000];
 
 async function makeBids(bestBidPrices, allOrders, position, balances) {
     
