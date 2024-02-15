33f47a7bea88bd2d1a8aa5638b54d7bc08362b09
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2024-02-15 13:44:54 +0000

    Increment up order size again.

--- replaceSCoinOrders.js
+++ replaceSCoinOrders.js
@@ -27,7 +27,7 @@ This is to reduce the impact of sharp price moves where the proce shoots through
 remains at high/low levels for some period of time.  In this scenario we become 
 oversold/overbought too quickly. 
 */
-const qtyLadder = [27, 23, 19, 15, 13];  
+const qtyLadder = [29, 25, 19, 15, 13];  
 
 const threshold = { 
     overSold : 300, 
