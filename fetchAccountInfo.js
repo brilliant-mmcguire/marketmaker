b83260e763bf1e9c20d7d5f9ced683d735e5e767
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2024-02-24 23:00:17 +0000

    Use 3d weighted avg price for market to market of asset balances.

--- fetchAccountInfo.js
+++ fetchAccountInfo.js
@@ -6,6 +6,8 @@ https://binance-docs.github.io/apidocs/spot/en/#spot-account-endpoints
 Do we care about recent trading activity, average prices, p&l?
 */
 
+
+const { fetchPriceStats } = require('./marketDataTxns');
 const crypto = require('crypto');
 const cfg = require('dotenv').config();
 const API_SECRET = process.env.API_SECRET;
@@ -16,25 +18,31 @@ function filterByAsset(asset, price, accountInfo){
     let b = accountInfo.balances.filter(balance => (balance.asset==asset))[0];
     return {
         qty  : b.total, 
-        free : b.free,
-        usd : Math.round(100*b.total*price)/100  
+        usd : Math.round(100*b.total*price)/100,
+        free : Math.round(100*b.free*price)/100
     };
 }
 async function main() {
     try {
+        const prcWindow = '3d';
         const noneZeroBalances =  await fetchAccountInfo();
+        const prcETH = await fetchPriceStats('ETHUSDT', prcWindow);
+        const prcBTC = await fetchPriceStats('BTCUSDT', prcWindow);
+        const prcBNB = await fetchPriceStats('BNBUSDT', prcWindow);
+        const prcXRP = await fetchPriceStats('XRPUSDT', prcWindow);
 
         let balances = {
-           USDC : filterByAsset('USDC',    1.00, noneZeroBalances),
-           USDT : filterByAsset('USDT',    1.00, noneZeroBalances), 
-           ETH  : filterByAsset('ETH',  2700.00, noneZeroBalances),
-           BTC  : filterByAsset('BTC', 50000.00, noneZeroBalances), 
-           BNB  : filterByAsset('BNB',   333.00, noneZeroBalances),
-           XRP  : filterByAsset('XRP',     0.56, noneZeroBalances)
+           USDC : filterByAsset('USDC', 1.00, noneZeroBalances),
+           USDT : filterByAsset('USDT', 1.00, noneZeroBalances), 
+           ETH  : filterByAsset('ETH', prcETH.weightedAvgPrice, noneZeroBalances),
+           BTC  : filterByAsset('BTC', prcBTC.weightedAvgPrice, noneZeroBalances), 
+           BNB  : filterByAsset('BNB', prcBNB.weightedAvgPrice, noneZeroBalances),
+           XRP  : filterByAsset('XRP', prcXRP.weightedAvgPrice, noneZeroBalances)
         }
         
         var b = Object.values(balances)
         let totalUsd = b.reduce((acc, item) => acc + item.usd, 0);
+        totalUsd =  Math.round(100*totalUsd)/100;
 
         console.log(`Balances for uid ${noneZeroBalances.uid} @ `, new Date());
         console.log(`total: ${totalUsd}`);
