2c72d12a32c629b0ff740070845955fda33ee612
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2024-02-29 07:51:36 +0000

    Ratchet up level 1 order sizes.

--- replaceSCoinOrders.js
+++ replaceSCoinOrders.js
@@ -27,7 +27,7 @@ This is to reduce the impact of sharp price moves where the proce shoots through
 remains at high/low levels for some period of time.  In this scenario we become 
 oversold/overbought too quickly. 
 */
-const qtyLadder = [97, 61, 47, 37, 29];  
+const qtyLadder = [97, 71, 47, 37, 29];  
 
 const threshold = { 
     overSold : 800, 
