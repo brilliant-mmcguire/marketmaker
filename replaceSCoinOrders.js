8d9f361bfd829eb18ff4986cc3965756ac520176
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2024-03-01 12:49:56 +0000

    Ratchet up order sizes.

--- replaceSCoinOrders.js
+++ replaceSCoinOrders.js
@@ -27,7 +27,7 @@ This is to reduce the impact of sharp price moves where the proce shoots through
 remains at high/low levels for some period of time.  In this scenario we become 
 oversold/overbought too quickly. 
 */
-const qtyLadder = [107, 79, 53, 37, 29];  
+const qtyLadder = [127, 83, 59, 41, 29];  
 
 const threshold = { 
     overSold : 800, 
