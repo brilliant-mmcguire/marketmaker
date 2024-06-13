1555a02675fa42c2b881a49e8398b3c85a56d064
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2024-06-13 08:00:08 +0100

    Decrease order qty to see if traded volume decreases.

--- replaceSCoinOrders.js
+++ replaceSCoinOrders.js
@@ -28,7 +28,7 @@ This is to reduce the impact of sharp price moves where the proce shoots through
 remains at high/low levels for some period of time.  In this scenario we become 
 oversold/overbought too quickly. 
 */
-const qtyLadder = [113, 109, 107, 31, 19, 13, 11];  
+const qtyLadder = [83, 79, 73, 31, 19, 13, 11];  
 
 const tickSize = 0.0001;  // Tick Size is 1 basis point.
 const posLimit = 900  // aim to remain inside targetQ +- posLimit
