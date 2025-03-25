1715267673ed934c847a5fe352a43ba1c0746ad7
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2025-03-25 08:57:27 +0000

    Increment up order size.

--- replaceSCoinOrders.js
+++ replaceSCoinOrders.js
@@ -28,7 +28,7 @@ This is to reduce the impact of sharp price moves where the proce shoots through
 remains at high/low levels for some period of time.  In this scenario we become 
 oversold/overbought too quickly. 
 */
-const qtyMax = 99;
+const qtyMax = 129;
 
 const tickSize = 0.0001;  // Tick Size is 1 basis point.
 const posLimit = 900  // aim to remain inside targetQ +- posLimit
