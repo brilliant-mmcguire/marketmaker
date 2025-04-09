672be8bdc5dd919235692974d86d8a4ce990c34c
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2025-04-09 09:04:16 +0100

    Slow down time it takes to forget our trade prices.

--- replaceSCoinOrders.js
+++ replaceSCoinOrders.js
@@ -139,10 +139,10 @@ function quoteQuota(mktQuoteSize) {
 
 function taperTradePrice(tradePrice, tradeAge, mktPrice) {
     // Weight our avg trade price with the market price depending on the age of our trades. 
-    // If we have'd traded for a while (up to 2 hours), 
+    // If we have'd traded for a while (up to lifeTime hours), 
     // we tend to the hourly weighted market price.   
     const lifeTime = 4.0; // hours.
-    const ageScalar = Math.sqrt(Math.max(2.0 - tradeAge,0)/2.0); 
+    const ageScalar = Math.sqrt(Math.max(2.0 - tradeAge,0)/lifeTime); 
     console.assert(ageScalar<=1.0 && ageScalar >=0.0 ,`0 <= trade age scalar <= 1`);
     return ageScalar*tradePrice + (1.0-ageScalar)*mktPrice; 
 }
