694a9895eae8ec9e9400171bf7ae298e9f758294
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2025-08-29 20:42:56 +0100

    Narrow range within which price is assumed to mean revert.

--- replaceSCoinOrders.js
+++ replaceSCoinOrders.js
@@ -25,8 +25,8 @@ const tickSize = 0.0001;  // Tick Size is 1 basis point.
 const posLimit = 1200  // aim to remain inside targetQ +- posLimit
 
 const target = {
-    hiPrice : 1.0007,  //
-    loPrice : 0.9991,  // 
+    hiPrice : 1.0006,  //
+    loPrice : 0.9992,  // 
     hiQty : 1000, // Hold less USDC when its price is high in anticipation of mean reversion.  
     loQty : 3000, // Buy more USDC when its price is low. 
 };
