546efb29af3671019ed6641e62d4a6c2dd88950f
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2025-09-04 21:49:05 +0100

    Adjustment only when moving away from best price.

--- replaceSCoinOrders.js
+++ replaceSCoinOrders.js
@@ -112,8 +112,7 @@ function quotePriceAdjustment(normalisedDeviation) {
         -0.5  +0.25
         -1.0  +2.00
 -       -1.5  +6.75 */
-    // return -2.0 * tickSize * normalisedDeviation**3;
-    return 0;
+    return -2.0 * tickSize * normalisedDeviation**3;
 }
 
 function scaleOrderQty(balances) {
@@ -186,7 +185,7 @@ function stochasticDecision(orderCount) {
 function calculateBidCeiling(mktQuotes, params, target, tickSize) {
     let taperPrice = params.avgBuy.taperPrice;
     let bidCeiling = Math.min(taperPrice, params.mktPrice);
-    let adjustment = quotePriceAdjustment(params.deviation);
+    let adjustment = Math.min(quotePriceAdjustment(params.deviation),0);
     bidCeiling += adjustment;
   
     // If market price is above our high target, be more conservative.
@@ -217,7 +216,7 @@ function calculateBidCeiling(mktQuotes, params, target, tickSize) {
 function calculateOfferFloor(mktQuotes, params, target, tickSize) {
     let taperPrice = params.avgSell.taperPrice;
     let offerFloor = Math.max(taperPrice, params.mktPrice);
-    let adjustment = quotePriceAdjustment(params.deviation);
+    let adjustment = Math.max(quotePriceAdjustment(params.deviation),0);
     offerFloor += adjustment;
 
     // If market price is below our low target, be more conservative
