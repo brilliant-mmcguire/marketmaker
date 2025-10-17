5896f5c76b42b0bf73f9c156be04846ba6198f58
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2025-10-17 22:15:42 +0100

    Tighten hi lo range.

--- replaceSCoinOrders.js
+++ replaceSCoinOrders.js
@@ -25,7 +25,7 @@ const tickSize = 0.0001;  // Tick Size is 1 basis point.
 const posLimit = 1200  // aim to remain inside targetQ +- posLimit
 
 const target = {
-    hiPrice : 1.0010,  //
+    hiPrice : 1.0006,  //
     loPrice : 0.9990,  // 
     hiQty   : 1000, // Hold less USDC when its price is high in anticipation of mean reversion.  
     loQty   : 3000, // Buy more USDC when its price is low. 
