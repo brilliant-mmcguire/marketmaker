bb25367b1662d87ef0ed630cd60f64d2b5482a46
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2025-11-21 15:46:23 +0000

    Taking a risk with raised bid ceiling.

--- replaceSCoinOrders.js
+++ replaceSCoinOrders.js
@@ -25,7 +25,7 @@ const tickSize = 0.0001;  // Tick Size is 1 basis point.
 const posLimit = 1200  // aim to remain inside targetQ +- posLimit
 
 const target = {
-    hiPrice : 1.00072,  //
+    hiPrice : 1.00062,  //
     loPrice : 0.99925,  // 
     hiQty   : 1000, // Hold less USDC when its price is high in anticipation of mean reversion.  
     loQty   : 3000, // Buy more USDC when its price is low. 
