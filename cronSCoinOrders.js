54c2449ff39fe097a11d8dca541e168bc4ab0951
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2025-12-11 15:46:38 +0000

    Remove dead code

--- cronSCoinOrders.js
+++ cronSCoinOrders.js
@@ -1,15 +1,13 @@
 const cron = require('node-cron');
 const { placeSCoinOrders } = require('./replaceSCoinOrders');
-const { cancelStaleOrders } = require('./orderTxns');
 
 console.log("Starting cron: placing StableCoin orders for USDCUSCT."); 
 
-// Schedule the task to run every 3 minutes.
+// Schedule the task to run every 1 minute.
 const job = cron.schedule('*/1 * * * *', async () => {
     console.log('Invoking place orders at ' + new Date().toLocaleString());
     try {
-        //await cancelStaleOrders('USDCUSDT');  
-        await placeSCoinOrders();   
+        await placeSCoinOrders();  
     } catch (error) {    
         console.error(`Error placing orders: ${error}`);
     }
