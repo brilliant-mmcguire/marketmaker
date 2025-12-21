806133db5e6e93f8e02de428fd99fa818bab218d
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2025-12-21 17:29:55 +0000

    Low bid ceiling.

--- replaceSCoinOrders.js
+++ replaceSCoinOrders.js
@@ -41,7 +41,7 @@ const target = {
 
 
 const target = {
-    hiPrice : 1.00035,  //
+    hiPrice : 1.00015,  //
     loPrice : 0.99925,  // 
     hiQty   : 1000, // Hold less SCoin when its price is high in anticipation of mean reversion.  
     loQty   : 3000, // Buy more SCoin when its price is low. 
