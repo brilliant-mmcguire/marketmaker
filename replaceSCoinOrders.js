c2738d1c66bc7deb8fbe9a754375601e3dc198d0
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2026-02-06 11:49:54 +0000

    Increment up USDC ceiling.

--- replaceSCoinOrders.js
+++ replaceSCoinOrders.js
@@ -39,7 +39,7 @@ const target = {
 */
 
 const target = {
-    hiPrice : 1.00112,  //
+    hiPrice : 1.00122,  //
     loPrice : 0.99925,  // 
     hiQty   : 400, // Hold less SCoin when its price is high in anticipation of mean reversion.  
     loQty   : 1600, // Buy more SCoin when its price is low. 
