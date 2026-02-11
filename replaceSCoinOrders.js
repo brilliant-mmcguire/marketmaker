5e1f7c54633dcd8a94cfaf7ad33145fcfde2ac64
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2026-02-11 09:24:55 +0000

    Increase quantity if USDC FDUSD held a little bit.

--- replaceSCoinOrders.js
+++ replaceSCoinOrders.js
@@ -42,7 +42,7 @@ const target = {
     hiPrice : 1.00122,  //
     loPrice : 0.99925,  // 
     hiQty   : 400, // Hold less SCoin when its price is high in anticipation of mean reversion.  
-    loQty   : 1600, // Buy more SCoin when its price is low. 
+    loQty   : 2200, // Buy more SCoin when its price is low. 
 };
 
 /*
