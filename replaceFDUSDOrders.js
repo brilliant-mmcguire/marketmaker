c665dcb2de50291091df9ec3b262552b8337bb06
brilliant-mmcguire <mmcguire@brilliantbunch.net>
2026-08-27 12:35:09 +0100

    Revert tick size.

--- replaceFDUSDOrders.js
+++ replaceFDUSDOrders.js
@@ -21,7 +21,7 @@ oversold/overbought too quickly.
 const qtyMax =  129;
 const qtyMin =  11;
 
-const tickSize = 0.00001;  // Tick Size is 1 basis point.
+const tickSize = 0.0001;  // Tick Size is 1 basis point.
 const posLimit = 400  // aim to remain inside targetQ +- posLimit
 
 const target = {
